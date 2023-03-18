a066884eadfea5f476262b9148c2c2aec4d11d03
kybe236 <kyrill.belomenov@outlook.de>
2023-03-18 17:02:36 +0100

    init

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-from fastapi import FastAPI, Query
+# Base FastAPI
+from fastapi import FastAPI
+# pydantic BaseModel
+from pydantic import BaseModel
+# Response types
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+# Cookies etc
+from fastapi import Cookie
+# Response
+from fastapi import Response
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root(name: list[str] | None = Query(default=None, max_length=20, min_length=3)):
-    return {"name": name}
+class Image(BaseModel):
+    url: str
+    name: str
+
+
+class Base(BaseModel):
+    name: str
+    old: int
+    etc: set[str] = []
+    image: Image
+
+
+@app.get("/test")
+def api(ads_id: str | None = Cookie(default=None)) -> Response:
+    return JSONResponse(content={"hi": ads_id})
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return f"""
+            <html>
+                <head>
+                    <title>Rock paper scissor api</title>
+                </head>
+                <body>
+                    <h1>Rock paper scissor api</h1>
+                </body>
+            </html>
+            """
+
+
+@app.get("/discord", response_class=RedirectResponse)
+def read_root():
+    return "https://discord.gg/v9D5VD4Rfp"
+
