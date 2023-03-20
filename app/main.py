610501d9b2c992f61623b287fe0fb8279d2fb2a6
kybe236 <kyrill.belomenov@outlook.de>
2023-03-20 13:52:51 +0100

    moved to app

--- app/main.py
+++ app/main.py
@@ -45,26 +45,6 @@ async def read_root():
             """
 
 
-@app.get("/discord",
-         response_class=RedirectResponse,
-         tags=[Tags.user, Tags.redirect],
-         summary="redirect to discord",
-         description="redirect to the rock-paper-scissor-api server",
-         response_description="discord server invite URl redirect")
-async def discord():
-    return "https://discord.gg/v9D5VD4Rfp"
-
-
-@app.get("/youtube",
-         response_class=RedirectResponse,
-         tags=[Tags.user, Tags.redirect],
-         summary="redirect to youtube",
-         description="redirect to the youtube channel",
-         response_description="youtube URL redirect")
-async def youtube():
-    return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-
-
 @app.get("/favicon.ico",
          response_class=FileResponse,
          tags=[Tags.style],
